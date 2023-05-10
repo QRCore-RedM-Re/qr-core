b161d91bee2ba6488761743dd5a233bdef9598b5
xThrasherrr <101474430+xThrasherrr@users.noreply.github.com>
2023-05-09 20:35:55 -0600

    fix: get cache.ped health

--- client/loops.lua
+++ client/loops.lua
@@ -13,7 +13,7 @@ CreateThread(function()
     while true do
         if LocalPlayer.state.isLoggedIn then
             if (QRCore.PlayerData.metadata.hunger <= 0 or QRCore.PlayerData.metadata.thirst <= 0) and not QRCore.PlayerData.metadata.isdead then
-                local currentHealth = GetEntityHealth(ped)
+                local currentHealth = GetEntityHealth(cache.ped)
                 local decreaseThreshold = math.random(5, 10)
                 SetEntityHealth(cache.ped, currentHealth - decreaseThreshold)
             end
