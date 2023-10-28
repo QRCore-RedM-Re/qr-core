51813437e6fdda65148aa2cc5eb2ce243f0c7b07
xThrasherrr <thrashergamezz@gmail.com>
2023-10-28 14:44:59 -0600

    fix: DrawText3D

--- client/functions.lua
+++ client/functions.lua
@@ -33,19 +33,15 @@ function QRCore.Functions.DrawText(x, y, width, height, scale, r, g, b, a, text)
 end
 
 function QRCore.Functions.DrawText3D(x, y, z, text)
-    -- Use local function instead
+    local onScreen, _x , _y = GetScreenCoordFromWorldCoord(x, y, z)
+
     SetTextScale(0.35, 0.35)
-    SetTextFont(4)
-    SetTextProportional(1)
-    SetTextColour(255, 255, 255, 215)
-    SetTextEntry('STRING')
-    SetTextCentre(true)
-    AddTextComponentString(text)
-    SetDrawOrigin(x, y, z, 0)
-    DrawText(0.0, 0.0)
-    local factor = (string.len(text)) / 370
-    DrawRect(0.0, 0.0 + 0.0125, 0.017 + factor, 0.03, 0, 0, 0, 75)
-    ClearDrawOrigin()
+    SetTextFontForCurrentCommand(1)
+    SetTextColor(255, 255, 255, 215)
+    local str = CreateVarString(10, "LITERAL_STRING", text, Citizen.ResultAsLong())
+    SetTextCentre(1)
+    DisplayText(str, _x, _y)
+    local factor = (string.len(text)) / 150
 end
 
 QRCore.Functions.RequestAnimDict = lib.requestAnimDict
